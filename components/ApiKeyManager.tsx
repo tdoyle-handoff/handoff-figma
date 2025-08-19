2566c1cdf4cc9031e404b10a70dd1906885f369e
Taylor <ttaylordoyle@gmail.com>
2025-08-18 20:09:34 -0400

    push

--- components/ApiKeyManager.tsx
+++ components/ApiKeyManager.tsx
@@ -18,7 +18,7 @@ import {
   TestTube,
   Wrench
 } from 'lucide-react';
-import { toast } from 'sonner@2.0.3';
+import { toast } from 'sonner';
 import { projectId, publicAnonKey } from '../utils/supabase/info';
 
 interface ApiKeyStatus {
